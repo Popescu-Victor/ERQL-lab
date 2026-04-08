6d6c4ebfde47e66f924280bd92acae33d2f89dd2
Popescu_Victor <romaniahasmetal@gmail.com>
2026-04-08 21:26:48 +0300

    Tested GUI in Rust

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use eframe::egui;
+
+fn main() -> eframe::Result<()> {
+    eframe::run_native(
+        "My App",
+        eframe::NativeOptions::default(),
+        Box::new(|_cc| Ok(Box::new(MyApp::default()))),
+    )
+}
+
+#[derive(Default)]
+struct MyApp {
+    name: String,
+    count: u32,
+}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello, egui!");
+            ui.horizontal(|ui| {
+                ui.label("Your name:");
+                ui.text_edit_singleline(&mut self.name);
+            });
+            if ui.button("Click me").clicked() {
+                self.count += 1;
+            }
+            ui.label(format!("Hello {}, clicked {} times", self.name, self.count));
+        });
+    }
+}
