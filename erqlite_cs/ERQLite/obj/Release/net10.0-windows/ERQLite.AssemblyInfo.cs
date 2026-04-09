83b3b99c28d481da28e8f9c9272d1d1f5ebb73bd
Popescu_Victor <romaniahasmetal@gmail.com>
2026-04-09 19:13:27 +0300

    Recreated the original Tkinter interface in Visual Studio

--- erqlite_cs/ERQLite/obj/Release/net10.0-windows/ERQLite.AssemblyInfo.cs
+++ erqlite_cs/ERQLite/obj/Release/net10.0-windows/ERQLite.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ERQLite")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b47418b6540592d19d2fa6b8d091a68c60f26e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ERQLite")]
