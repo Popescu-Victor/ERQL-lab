f87af0646ea26f1f081b8979cf30a00b001eec70
Popescu_Victor <romaniahasmetal@gmail.com>
2026-04-12 10:33:14 +0300

    Filled in scipy formulas from notes

--- ml-lab/cheatsheet.py
+++ ml-lab/cheatsheet.py
@@ -1,7 +1,12 @@
 import numpy as np
 import pandas as pd
-from scipy import stats
+import scipy
 from sklearn.model_selection import train_test_split
 import modin
 import tensorflow as tf
 
+scipy.stats.binom.pmf(x, n, p) # x = value of interest, n = number of trials, p = possibility of success
+
+scipy.stats.norm.cdf(x, loc, scale) # loc = mean of the probability distribution, scale = standard dev
+
+scipy.stats.poisson.pmf (expected_value, probability)
