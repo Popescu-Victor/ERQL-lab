94c393706829efeb1e0f809b6fb1a48acfcafd32
Popescu Victor <romaniahasmetal@gmail.com>
2026-04-27 12:09:34 +0300

    def third_col() for creating an additional type of data for each dataset.

--- simpsons_paradox/fake_dataset_generation.py
+++ simpsons_paradox/fake_dataset_generation.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def generate_inverse_correlation(
     mean_x: float, std_x: float,
@@ -23,47 +25,52 @@ def generate_inverse_correlation(
     
     return x, y
 
-std_dev = 10
-
-import matplotlib.pyplot as plt
+std_dev = 5
+corr = -0.6
 
 
 c, d = generate_inverse_correlation(
         mean_x=40, std_x=std_dev,
-        mean_y=40, std_y=std_dev,
+        mean_y=55, std_y=std_dev,
         size=100,
-        correlation=-0.8
+        correlation=corr
 )
-
-
 
 a, b = generate_inverse_correlation(
-        mean_x=30, std_x=std_dev,
-        mean_y=30, std_y=std_dev,
+        mean_x=35, std_x=std_dev,
+        mean_y=50, std_y=std_dev,
         size=100,
-        correlation=-0.7
+        correlation=corr
 )
 
-
 e, f = generate_inverse_correlation(
-        mean_x=20, std_x=std_dev,
-        mean_y=20, std_y=std_dev,
+        mean_x=25, std_x=std_dev,
+        mean_y=45, std_y=std_dev,
         size=100,
-        correlation=-0.7
+        correlation=corr
 )
 
 g, h = generate_inverse_correlation(
-        mean_x=10, std_x=std_dev,
-        mean_y=10, std_y=std_dev,
+        mean_x=20, std_x=std_dev,
+        mean_y=40, std_y=std_dev,
         size=100,
-        correlation=-0.7
+        correlation=corr
+
 )
 
 
+def third_col(col, data):
+    df = []
+    for i in range(len(col)):
+        df.append(data)
+    return df
 
-plt.scatter(a, b, alpha=0.5, color='grey')
+
+print(np.column_stack([a, b, third_col(a, 1)]))
+
+plt.scatter(a, b, alpha=0.5, color='steelblue')
 plt.scatter(c, d, alpha=0.5, color='steelblue')
-plt.scatter(e, f, alpha=0.5, color='coral')
-plt.scatter(g, h, alpha=0.5, color='lightgreen')
+plt.scatter(e, f, alpha=0.5, color='steelblue')
+plt.scatter(g, h, alpha=0.5, color='steelblue')
 plt.legend()
 plt.show()
