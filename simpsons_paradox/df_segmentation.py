81d0f93042d3c7947c118ecc149182c8df119187
Popescu Victor <romaniahasmetal@gmail.com>
2026-04-30 10:02:02 +0300

    Clean dataset traits correlation

--- simpsons_paradox/df_segmentation.py
+++ simpsons_paradox/df_segmentation.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 df = pd.read_csv('simpsons_paradox/dataset.csv')
 
@@ -7,3 +10,6 @@ groups = {value: group for value, group in df.groupby(df.columns[-1])}
 
 for value, group_df in groups.items():
     print(value, group_df.shape)
+
+sns.regplot(x=df.iloc[:, 0], y=df.iloc[:, 1], scatter_kws={'alpha':0.5}, line_kws={'color':'red', 'linewidth':1.5})
+plt.show()
