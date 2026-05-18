cb74c9a6f5c51a23a9413500716970e8d1c4152e
Popescu Victor <romaniahasmetal@gmail.com>
2026-05-18 12:27:51 +0300

    Printing out every unique user ID

--- firestore/anon_user_id.py
+++ firestore/anon_user_id.py
@@ -9,6 +9,9 @@ import os
 load_dotenv()
 folder_path = os.getenv("FOLDER")
 
+user_list = []
+
+
 for filename in os.listdir(folder_path):
 
     if filename.endswith(".xlsx") and not filename.startswith("~$"): # and not filename.startswith("~$") - This part skips temp files in folder created by opening excel files
@@ -19,5 +22,12 @@ for filename in os.listdir(folder_path):
         for col in df.columns:
             if df[col].astype(str).str.contains("Test Passes", na=False).any():
                 column_index = df.columns.get_loc(col)
-                print(f"Found column index: {column_index}")
-                break
+                for index, value in df[col].items():
+                    if "Test Passes for Participant: " in str(value):
+                        anon_user_id = value.split("Test Passes for Participant: ")[1]
+                        if anon_user_id not in user_list:
+                            user_list.append(anon_user_id)
+
+print("Unique Anonymous User IDs:")
+for user_id in user_list:
+    print(user_id)
