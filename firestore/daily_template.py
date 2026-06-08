ff153a47d907fd4728257c16dd0110d1f38e7f47
Popescu Victor <romaniahasmetal@gmail.com>
2026-06-08 09:36:25 +0300

    Save Daily Template file

--- firestore/daily_template.py
+++ firestore/daily_template.py
@@ -1,12 +1,13 @@
-class DailyTemplate(gym, reading, socializing, coding, chores, sleep, diet):
-    def __init__(self, gym, reading, socializing, coding, chores, sleep, diet):
-        self.gym = gym
-        self.reading = reading
-        self.socializing = socializing
-        self.coding = coding
-        self.chores = chores
-        self.sleep = sleep
-        self.diet = diet
+class DailyTemplate(gym, reading, socializing, coding, chores, sleep, diet_sugar, diet_protein):
+    def __init__(self, gym, reading, socializing, coding, chores, sleep, diet_sugar, diet_protein):
+        self.gym = gym # Bool | Did you go to the gym today?
+        self.reading = reading # Bool | Did you read today?
+        self.socializing = socializing # Bool | Did you socialize today?
+        self.coding = coding # Bool | Did you code today?
+        self.chores = chores # Bool | Did you do chores today?
+        self.sleep = sleep # Int | How many hours did you sleep tonight?
+        self.diet_sugar = diet_sugar # Bool | Did you consume too much sugar today?
+        self.diet_protein = diet_protein # Bool | Did you consume enough protein today?
 
     def run(self):
-        print(f"Today's yield: {self.gym + self.reading + self.socializing + self.coding + self.chores + self.sleep + self.diet}")
+        print(f"Today's yield: {self.gym + self.reading + self.socializing + self.coding + self.chores + self.sleep + self.diet_sugar + self.diet_protein}")
