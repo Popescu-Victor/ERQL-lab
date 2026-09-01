e7913ce79ca926e4e9f3b2927a90300e77fe0f44
Popescu-Victor <romaniahasmetal@gmail.com>
2026-09-01 14:36:34 +0300

    Grep-like function

--- cli-sandbox/cli_test.py
+++ cli-sandbox/cli_test.py
@@ -1,7 +1,14 @@
 import argparse
+from curses import echo
 from os import name
+from py_compile import main
 import selenium
 import dotenv
+import pandas as pd
+import keyring
+import subprocess
+import os
+
 
 parser = argparse.ArgumentParser(description="My CLI tool")
 parser.add_argument("login", help="Storing encrypted login credentials locally.")
@@ -10,7 +17,14 @@ def login_to_env():
     # Implement the logic to log in to the environment using Selenium
     pass
 
-def main():
+def str_finder(folder_path, target_string):
+    for i in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, i)):
+            with open(os.path.join(folder_path, i), 'r') as file:
+                if target_string in file.read():
+                    print(f"Found '{target_string}' in {i}")
+
+def     args = parser.parse_args()
     pass
 
 if name == "__main__":
